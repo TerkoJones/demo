85068c354998b46c090ba20b3fcfda57fb45f789
Terko Jones <terkojones@gmail.com>
2019-12-28 07:36:40 +0100

    otro

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,34 @@
 /// <reference types="node" />
 import { InspectOptions } from 'util';
-import { Writable } from 'stream';
-declare function replace(sandbox: object, message: string, options?: InspectOptions): string;
-declare namespace replace {
-    function writable(sandbox: object, writable: Writable, options?: InspectOptions): Writable;
+declare type Dictionary<T> = {
+    [key: string]: T;
+};
+interface LoggerBase extends InspectOptions {
+    encoding?: string;
+    flags?: string;
+    prompt?: string;
 }
-export = replace;
+interface LoggerOptions extends LoggerBase {
+    output: string;
+    date?: string | (() => string);
+}
+declare type LoggerDefs = Dictionary<LoggerOptions | string>;
+declare const $CONTEXT: unique symbol;
+interface Context {
+    [$CONTEXT]: boolean;
+    [key: string]: any;
+}
+interface LoggerFunction {
+    (...args: any[]): void;
+    (message?: string, ...args: any[]): void;
+    (context?: Context, message?: string, ...args: any[]): void;
+}
+interface Logger extends LoggerFunction {
+    [key: string]: LoggerFunction;
+}
+declare const logger: Logger;
+export default logger;
+export declare function registerLogger(name: string, output?: string): void;
+export declare function registerLogger(name: string, options?: LoggerOptions): void;
+export declare function registerLogger(defs: LoggerDefs): any;
+export declare function contextualize(obj: object): Context;
--- index.js
+++ index.js
@@ -1,81 +1,154 @@
 "use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
+var _a;
+Object.defineProperty(exports, "__esModule", { value: true });
 var util_1 = require("util");
-var vm_1 = require("vm");
-var stream_1 = require("stream");
-var REX_PH_STRING = /%%|%([\s\u0021-\u0024\u0026-\uffff]|%{2,})+%(?!%)/g;
-var REX_PH_STREAM = /(%%)|(%([\s\u0021-\u0024\u0026-\uffff]|%{2,})+%)|(%([\s\u0021-\u0024\u0026-\uffff]|%{2,})*$)/g;
-function replace(sandbox, message, options) {
-    REX_PH_STRING.lastIndex = 0;
-    vm_1.createContext(sandbox);
-    return message.replace(REX_PH_STRING, function (m) {
-        if (m === '%%')
-            return '%';
-        return parse_expression(sandbox, m, options);
-    });
-}
-function parse_expression(context, expr, options) {
-    console.log(expr);
-    expr = '(' + expr.substr(1, expr.length - 2).replace(/%%/g, '%') + ')';
-    var val = vm_1.runInContext(expr, context);
-    return options ? util_1.inspect(val, options) : val.toString();
-}
-var Replacer = (function (_super) {
-    __extends(Replacer, _super);
-    function Replacer(sandbox, destiny, options) {
-        var _this = _super.call(this) || this;
-        _this._writer = destiny;
-        _this._rest = '';
-        _this._sandbox = vm_1.createContext(sandbox);
-        _this._options = options;
-        return _this;
+var fs = require("fs");
+var path = require("path");
+var replace = require("mmo-replace");
+var $CONTEXT = Symbol('Context');
+var ENTRY_FOLDER = process.cwd();
+var DEFAULT_LOGGER_NAME = 'log';
+var CREATE_PROPERTY = 'extend';
+var DATE_PLACEHOLDERS = {
+    DA: 'getDay',
+    MO: 'getMonth',
+    YE: 'getFullYear',
+    HO: 'getHours',
+    MI: 'getMinutes',
+    SE: 'getSeconds',
+    ML: 'getMilliseconds'
+};
+var REX_PROPS = /[A-Z][A-Z]+/g;
+var REX_PH = /%%|%([\s\u0021-\u0024\u0026-\u01ff]|%{2,})+%/g;
+var WRITERS = {
+    stdout: process.stdout,
+    stderr: process.stderr
+};
+var LOGGERS = (_a = {},
+    _a[DEFAULT_LOGGER_NAME] = {
+        writer: process.stdout,
+        colors: true
+    },
+    _a);
+var DEFAULT_OPTIONS = {
+    encoding: 'utf8'
+};
+function write(message) {
+    var args = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        args[_i - 1] = arguments[_i];
     }
-    Replacer.prototype._write = function (chunk, encoding, callback) {
-        if (this._rest) {
-            chunk = this._rest + chunk.toString();
-            this._rest = '';
+    var prev = '';
+    if (this.prompt)
+        prev = this.prompt;
+    if (this.date)
+        prev += '[' + this.date() + ']';
+    if (prev)
+        prev += ':';
+    if (message) {
+        if (message[$CONTEXT]) {
+            var context = message;
+            message = args.shift();
+            if (message)
+                message = replace(context, message, this) + '\n';
+        }
+        else if (typeof message !== 'string') {
+            args.unshift(message);
+            message = '';
         }
         else {
-            chunk = chunk.toString();
+            message += '\n';
         }
-        var m;
-        var from = 0;
-        REX_PH_STREAM.lastIndex = 0;
-        while ((m = REX_PH_STREAM.exec(chunk)) != null) {
-            if (m.index > from)
-                this._writer.write(chunk.substr(from, m.index - from));
-            if (m[4]) {
-                console.log(m[0]);
-                this._rest = m[0];
-                from = chunk.length;
-                break;
-            }
-            this._writer.write(parse_expression(this._sandbox, m[0], this._options));
-            from = m.index + m[0].length;
-        }
-        if (from < chunk.length)
-            this._writer.write(chunk.substr(from));
-        callback();
-    };
-    return Replacer;
-}(stream_1.Writable));
-(function (replace) {
-    function writable(sandbox, writable, options) {
-        return new Replacer(sandbox, writable, options);
     }
-    replace.writable = writable;
-})(replace || (replace = {}));
-module.exports = replace;
+    else {
+        message = '';
+    }
+    this.writer.write(util_1.formatWithOptions.apply(void 0, __spreadArrays([this, prev + message], args)));
+}
+var logger = write.bind(LOGGERS[DEFAULT_LOGGER_NAME]);
+function register_logger(alias, options) {
+    var output;
+    if (typeof options === 'string') {
+        options = {
+            output: options
+        };
+    }
+    output = options.output;
+    var cur = LOGGERS[alias];
+    if (cur) {
+        Object.assign(cur, {
+            writer: register_writer(output, options)
+        }, options);
+    }
+    else {
+        cur = LOGGERS[alias] = Object.assign({
+            writer: register_writer(output, options)
+        }, DEFAULT_OPTIONS, options);
+        Object.defineProperty(logger, alias, {
+            value: write.bind(LOGGERS[alias]),
+            enumerable: true,
+            writable: false,
+            configurable: false
+        });
+    }
+    if (typeof options.date === 'string')
+        cur.date = parse_date(options.date);
+    if (cur.colors === undefined) {
+        cur.colors = (output === 'stdout' || output === 'stderr');
+    }
+    else {
+        cur.colors = (output === 'stdout' || output === 'stderr') ? cur.colors : false;
+    }
+}
+function register_writer(pathname, options) {
+    if (pathname === 'stderr' || pathname === 'stdout')
+        return WRITERS[pathname];
+    if (!path.isAbsolute(pathname))
+        pathname = path.resolve(ENTRY_FOLDER, pathname);
+    if (WRITERS[pathname])
+        return WRITERS[pathname];
+    return WRITERS[pathname] = fs.createWriteStream(pathname, options);
+}
+function parse_date(template) {
+    return function () {
+        var date = new Date(Date.now());
+        REX_PROPS.lastIndex = 0;
+        return template.replace(REX_PROPS, function (m) {
+            if (m.length === 2) {
+                var prop = DATE_PLACEHOLDERS[m];
+                if (prop) {
+                    var val = date[prop]().toString();
+                    val = m === 'Milliseconds' ? val.padStart(3, '0') : val.padStart(2, '0');
+                    return val;
+                }
+            }
+            return m;
+        });
+    };
+}
+exports.default = logger;
+function registerLogger(name, options) {
+    if (typeof name === 'string') {
+        register_logger(name, options);
+    }
+    else {
+        for (var k in name)
+            register_logger(k, name[k]);
+    }
+}
+exports.registerLogger = registerLogger;
+function contextualize(obj) {
+    var _a;
+    return Object.assign((_a = {},
+        _a[$CONTEXT] = true,
+        _a), obj);
+}
+exports.contextualize = contextualize;
 //# sourceMappingURL=index.js.map
